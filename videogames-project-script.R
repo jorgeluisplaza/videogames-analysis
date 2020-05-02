cd8b776f29cee5278751339aa3693883fec03f6f
Jorge <jorge.plaza@usach.cl>
2020-05-02 15:35:01 -0400

    Adding some comments

--- videogames-project-script.R
+++ videogames-project-script.R
@@ -1,10 +1,8 @@
-#library(tidyverse)
-#library(caret)
-#library(randomForest)
-#library(rpart)
-#library(factoextra)
-#library(data.table)
+###### VIDEOGAME PROJECT ##########
+###### JORGE PLAZA #########
 
+### INSTALL REQUIRED PACKAGES 
+### Will take a few seconds/minutes.
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
 if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
